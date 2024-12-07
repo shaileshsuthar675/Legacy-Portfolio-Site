43c1c91f75a289ac0b70e8132cb63383d0c200b4
shaileshsuthar675 <2019ume1168@mnit.ac.in>
2024-12-07 23:41:32 +0530

    added the clubfoot model initial page.

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template("main.html", keySkills=keySkills, ProfileInfo= ProfileInfo)
+    return render_template(
+        "/main/main.html",
+        keySkills=keySkills,
+        ProfileInfo=ProfileInfo,
+    )
 
 
 if __name__ == "__main__":
